aa59d75529b46fe941b198ac98a63afdc0e7181d
Cees-Jan Kiewiet <ceesjank@gmail.com>
2016-10-28 23:02:27 +0200

    ConsumerSecret test

---
+++ tests/Definition/ConsumerSecretTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace ApiClients\Tests\Tools\Psr7\Oauth1\Definition;
+
+use ApiClients\Tools\Psr7\Oauth1\Definition\ConsumerSecret;
+
+class ConsumerSecretTest extends \PHPUnit_Framework_TestCase
+{
+    public function testAccessToken()
+    {
+        $key = 'key';
+        $consumerSecret = new ConsumerSecret($key);
+        $this->assertSame($key, $consumerSecret->getConsumerSecret());
+        $this->assertSame($key, (string)$consumerSecret);
+    }
+}
