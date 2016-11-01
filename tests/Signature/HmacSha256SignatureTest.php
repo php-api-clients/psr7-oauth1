836ce734ef1caee4858f821e28dc2ba0297e65bd
gabidavila <gabidavila@gmail.com>
2016-11-01 19:39:59 -0200

    Added support to sign with hmac_sha256

---
+++ tests/Signature/HmacSha256SignatureTest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace ApiClients\Tests\Tools\Psr7\Oauth1\Signature;
+
+use ApiClients\Tools\Psr7\Oauth1\Definition\ConsumerSecret;
+use ApiClients\Tools\Psr7\Oauth1\Definition\TokenSecret;
+use ApiClients\Tools\Psr7\Oauth1\Signature\HmacSha256Signature;
+use GuzzleHttp\Psr7\Uri;
+use Psr\Http\Message\UriInterface;
+
+class HmacSha256SignatureTest extends \PHPUnit_Framework_TestCase
+{
+    public function testGetMethod()
+    {
+        $this->assertSame('HMAC-SHA256', (new HmacSha256Signature(new ConsumerSecret('secret')))->getMethod());
+    }
+
+    public function provideSign()
+    {
+        return [
+            [
+                new Uri('https://example.com/'),
+                'x0e2N4wRpr2UB+rX8mwnY01tp6fnXT5SE36kAvbdSYo=',
+                'ovXZ33UaQylAO8L8Ad1f/TwLU1YBXEygau34O7sDSw0=',
+            ],
+            [
+                new Uri('https://example.com/thea.pot',[], 'THEAPOT'),
+                'u5nNam6Cn+fCko38qAgxWUYmRrPPTF5OFOC9wZ4LHRg=',
+                '8IQ1uD3ZR+b8U0Fd++pGGzqNmKiF94z3psbsa+M0SxQ=',
+            ],
+            [
+                new Uri('https://example.com/?foo=bar', ['foo' => 'bar',]),
+                'SlDm7qfJO/mxl1Ewnewj46jjbuCEKYGNTeew2uMDwPM=',
+                '5vVeCSAcL52LVGWWQ1pUFUe7cpVwkhreMQMck3PEw+E=',
+            ],
+            [
+                new Uri('https://example.com/',['foo' => 'bar',], 'HEAD'),
+                'x0e2N4wRpr2UB+rX8mwnY01tp6fnXT5SE36kAvbdSYo=',
+                'ovXZ33UaQylAO8L8Ad1f/TwLU1YBXEygau34O7sDSw0=',
+            ],
+        ];
+    }
+
+    /**
+     * @param UriInterface $uri
+     * @param $signedSignature
+     * @dataProvider provideSign
+     */
+    public function testSign(UriInterface $uri, $signedSignature, $signedTokenSecretSignature)
+    {
+        $secret = new ConsumerSecret('consumerSecret');
+        $signature = new HmacSha256Signature($secret);
+        $tokenSecret = new TokenSecret('tokenSecret');
+        $this->assertSame($signedSignature, $signature->sign($uri));
+        $signature = $signature->withTokenSecret($tokenSecret);
+        $this->assertSame($signedTokenSecretSignature, $signature->sign($uri));
+        $signature = $signature->withoutTokenSecret();
+        $this->assertSame($signedSignature, $signature->sign($uri));
+    }
+}
