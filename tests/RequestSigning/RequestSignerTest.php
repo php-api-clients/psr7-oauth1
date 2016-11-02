fba333f02b763fc36f96773440ed9f2a0ff4807d
Cees-Jan Kiewiet <ceesjank@gmail.com>
2016-11-02 07:58:44 +0100

    RequestSigner::signToRequestAuthorization test

--- tests/RequestSigning/RequestSignerTest.php
+++ tests/RequestSigning/RequestSignerTest.php
@@ -75,4 +75,50 @@ class RequestSignerTest extends \PHPUnit_Framework_TestCase
             $this->assertTrue($expectedHeaderPart);
         }
     }
+
+    public function testSignToRequestAuthorization()
+    {
+        $callbackUri = 'https://example.com/callback';
+        $expectedHeaderParts = [
+            'oauth_consumer_key' => false,
+            'oauth_nonce' => false,
+            'oauth_signature_method' => false,
+            'oauth_timestamp' => false,
+            'oauth_version' => false,
+            'oauth_callback' => false,
+            'oauth_signature' => false,
+        ];
+        $request = new Request(
+            'POST',
+            'httpx://example.com/',
+            [
+                'Content-Type' => 'application/x-www-form-urlencoded',
+            ]
+        );
+        $requestSigner = new RequestSigner(
+            new ConsumerKey('consumer_key'),
+            new ConsumerSecret('consumer_secret')
+        );
+
+        $signedRequest = $requestSigner->signToRequestAuthorization($request, $callbackUri);
+
+        $this->assertNotSame($request, $signedRequest);
+        $this->assertTrue($signedRequest->hasHeader('Authorization'));
+        $headerChunks = explode(' ', current($signedRequest->getHeader('Authorization')));
+        $this->assertCount(2, $headerChunks);
+        $this->assertSame('OAuth', $headerChunks[0]);
+
+        $headerChunks = explode(',', $headerChunks[1]);
+        $this->assertCount(count($expectedHeaderParts), $headerChunks);
+        foreach ($headerChunks as $headerChunk) {
+            list($key, $value) = explode('=', $headerChunk);
+            $this->assertTrue(isset($expectedHeaderParts[$key]));
+            $expectedHeaderParts[$key] = true;
+        }
+
+        foreach ($expectedHeaderParts as $expectedHeaderPart) {
+            $this->assertInternalType('bool', $expectedHeaderPart);
+            $this->assertTrue($expectedHeaderPart);
+        }
+    }
 }
